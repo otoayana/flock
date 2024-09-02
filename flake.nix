efeaed46694c820d82cb1d80e0b86fc825b68835
Lux Aliaga <me@lux.rip>
2024-09-02 13:01:05 -0400

    flake: include naersk
    the flake was also missing some dependencies that were used within the
    project. add those too.

--- flake.nix
+++ flake.nix
@@ -2,20 +2,51 @@
   description = "development shell for flock";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
+    naersk.url = "github:nix-community/naersk";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
   outputs = {
     self,
+    fenix,
+    naersk,
     nixpkgs,
     utils,
   }:
     utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages."${system}";
-    in {
-      devShell = pkgs.mkShell {
-        nativeBuildInputs = with pkgs; [rustup cargo-deny cargo-outdated sqlite rlwrap];
+      pkgs = (import nixpkgs) {
+        inherit system;
       };
+
+      naersk' = pkgs.callPackage naersk { };
+      toolchain = fenix.packages.${system}.minimal.toolchain;
+    in {
+      #packages.x86_64-linux.default = fenix.packages.x86_64-linux.default.toolchain;
+      defaultPackage = naersk'.buildPackage {
+          src = ./.;
+      };
+
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = [
+          toolchain
+          (with pkgs; [
+            gcc
+            rustc
+            cargo
+            cargo-deny
+            cargo-outdated
+            openssl.dev
+            pkg-config
+            rlwrap
+            rust-analyzer
+            sqlite
+          ])
+        ];
+        };
     });
 }
